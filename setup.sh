c7aaccbb22b91c65238754a7f61979067b0ee039
Subroh75 <subroh.iyer@gmail.com>
2026-03-25 10:10:18 +1100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial launch of Oracle v18.0"
+git branch -M main
+git remote add origin https://github.com/subroh75/nifty-500-oracle.git
+git push -u origin main
